af41ef7b3247dc393b310deb767d72ed4cc10cdd
fatemeh.afshani <fatemeh.afshani78@gmail.com>
2025-11-22 21:49:44 +0330

    feat: calculate a very basic output

--- code/code.py
+++ code/code.py
@@ -1,7 +1,9 @@
-import sys
-import numpy as np
-import matplotlib
 
-print("python: ", sys.version)
-print("numpy", np.__version__)
-print("matplotlib", matplotlib.__version__)
+inputs = [1.2, 5.1, 2.1]
+weights = [3.1, 5.4, 2.8]
+bias = 3
+
+
+output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias
+
+print(output)
