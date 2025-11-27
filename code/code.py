9322a4baf59dcdf71a42f73592bcef6def6d9e7e
fatemeh.afshani <fatemeh.afshani78@gmail.com>
2025-11-27 16:47:19 +0330

    feat: add activation function

--- code/code.py
+++ code/code.py
@@ -1,14 +1,16 @@
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
 
-inputs = [
-    [1.2, 5.1, 2.1, 3],
-    [3.2, 2.1, 8, 1],
-    [2.2, 1.1, -1, 3],
-    ]
+nnfs.init()
+
+
+X, y = spiral_data(100, 3)
 
 
 class LayerDense:
     def __init__(self, n_inputs, n_outputs):
+        self.outputs = None
         self.weights = np.random.randn(n_inputs, n_outputs)
         self.biases = np.zeros((1, n_outputs))
         self.activation = None
@@ -22,10 +24,17 @@ class LayerDense:
         self.outputs = np.dot(inputs, self.weights) + self.biases
 
 
-layer1 = LayerDense(4, 5)
-layer2 = LayerDense(5, 2)
+class Activation:
+    def __init__(self):
+        self.output = None
 
-layer1.forward(inputs)
-layer2.forward(layer1.outputs)
+    def forward(self, inputData):
+        self.output = np.maximum(0, inputData)
 
-print(layer2.outputs)
+
+layer1 = LayerDense(2, 5)
+activation1 = Activation()
+
+layer1.forward(X)
+activation1.forward(layer1.outputs)
+print(activation1.output)
