a875202716f569569e52a6755391fe1aaa9d89c4
fatemeh.afshani <fatemeh.afshani78@gmail.com>
2025-11-27 11:11:19 +0330

    feat: minimal one layer nn

--- code/code.py
+++ code/code.py
@@ -5,12 +5,27 @@ inputs = [
     [3.2, 2.1, 8, 1],
     [2.2, 1.1, -1, 3],
     ]
-weights = [[3.1, 5.4, 2.8, 1],
-           [3.1, 5.4, 2.8, 2],
-           [3.1, 5.4, 2.8, 4]]
-
-bias = [2,3,4]
 
 
-output = np.dot(inputs, np.array(weights).T) + bias
-print(output)
+class LayerDense:
+    def __init__(self, n_inputs, n_outputs):
+        self.weights = np.random.randn(n_inputs, n_outputs)
+        self.biases = np.zeros((1, n_outputs))
+        self.activation = None
+        self.dropout = None
+        self.loss = None
+        self.accuracy = None
+        self.optimizer = None
+        self.learning_rate = None
+
+    def forward(self, inputs):
+        self.outputs = np.dot(inputs, self.weights) + self.biases
+
+
+layer1 = LayerDense(4, 5)
+layer2 = LayerDense(5, 2)
+
+layer1.forward(inputs)
+layer2.forward(layer1.outputs)
+
+print(layer2.outputs)
