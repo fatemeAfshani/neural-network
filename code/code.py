e6dabf352b2c3eaa2ba68b0844e195485d98f98c
fatemeh.afshani <fatemeh.afshani78@gmail.com>
2025-11-27 09:28:07 +0330

    feat: basic layer

--- code/code.py
+++ code/code.py
@@ -1,9 +1,16 @@
+import numpy as np
 
-inputs = [1.2, 5.1, 2.1]
-weights = [3.1, 5.4, 2.8]
-bias = 3
+inputs = [
+    [1.2, 5.1, 2.1, 3],
+    [3.2, 2.1, 8, 1],
+    [2.2, 1.1, -1, 3],
+    ]
+weights = [[3.1, 5.4, 2.8, 1],
+           [3.1, 5.4, 2.8, 2],
+           [3.1, 5.4, 2.8, 4]]
+
+bias = [2,3,4]
 
 
-output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias
-
+output = np.dot(inputs, np.array(weights).T) + bias
 print(output)
